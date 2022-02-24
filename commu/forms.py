b00e88dbc8b7fb6362b4dde81d36337b8103b038
dope66 <prince628@naver.com>
2022-02-24 14:11:33 +0900

    modif ,detail

--- commu/forms.py
+++ commu/forms.py
@@ -5,18 +5,18 @@ from django import forms
 
 class BoardForm(ModelForm):
     class Meta:
-        model =Board
-        fields=['b_title','b_content']
+        model = Board
+        fields = ['b_title', 'b_content']
 
-        labels={
+        labels = {
             'b_title': '글 제목',
-            'b_content':'글 내용'
+            'b_content': '글 내용'
         }
-        widgets={
-            'b_title':forms.TextInput(
+        widgets = {
+            'b_title': forms.TextInput(
                 attrs={
-                    'class':'form-control w-50',
-                    'placeholder':'제목을 써주세요'
+                    'class': 'form-control w-50',
+                    'placeholder': '제목을 써주세요'
                 }
             ),
             'b_content': forms.Textarea(
@@ -25,4 +25,17 @@ class BoardForm(ModelForm):
                     'placeholder': '내용을 써주세요'
                 }
             )
+        }
+
+
+class BoardDetailForm(forms.ModelForm):
+    class Meta:
+        model = Board
+        fields = "__all__"
+        labels={
+            'b_title': '글 제목',
+            'b_author': '글 작성자',
+            'b_content': '글 내용',
+            'b_like': '좋아요',
+            'b_date': '게시 날짜'
         }
--- commu/urls.py
+++ commu/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 
     path('list/', views.b_list, name='b_list'),
     path('create/', views.b_create, name='b_create'),
-
+    path('<int:board_id>/detail/',views.b_detail,name='b_detail'),
+    path('<int:pk>/modify/',views.b_modify,name="b_modify")
 ]
