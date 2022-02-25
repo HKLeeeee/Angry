1a67ddb81b5ca84e162abbda7715cc7d84bd0405
soheeeeeeee <soheeeee@kakao.com>
2022-02-25 16:43:59 +0900

    ADD signup function

--- user/forms.py
+++ user/forms.py
@@ -1,20 +1,20 @@
 from django import forms
 from user.models import Member
+from user.signupform.forms import UserCreationForm
 
 
-class SigninForm(forms.ModelForm):
+class SignupForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].help_text = ""
-        self.fields['password'].help_text = ""
 
     class Meta:
         model = Member
-        fields = ['email', 'password', 'username', 'last_name', 'first_name']
+        fields = ['username', 'email', 'nickname', 'last_name', 'first_name']
         labels = {
+            'username': '아이디',
             'email': '이메일',
-            'password': '비밀번호',
-            'username': '닉네임',
+            'nickname': '닉네임',
             'last_name': '이름',
             'first_name': ''
         }
@@ -25,16 +25,10 @@ class SigninForm(forms.ModelForm):
                     'placeholder': '이메일을 입력하세요'
                 }
             ),
-            'password': forms.PasswordInput(
-                attrs={
-                    'class': 'form-control w-50',
-                    'placeholder': '비밀번호를 입력하세요'
-                }
-            ),
             'username': forms.TextInput(
                 attrs={
                     'class': 'form-control w-50',
-                    'placeholder': '닉네임을 입력하세요'
+                    'placeholder': '아이디를 입력하세요'
                 }
             ),
             'last_name': forms.TextInput(
@@ -48,11 +42,22 @@ class SigninForm(forms.ModelForm):
                     'class': 'form-control w-50',
                     'placeholder': '이름을 입력하세요'
                 }
+            ),
+            'nickname': forms.TextInput(
+                attrs={
+                    'class': 'form-control w-50',
+                    'placeholder': '닉네임을 입력하세요'
+                }
             )
         }
 
 
 class LoginForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = ""
+        self.fields['password'].help_text = ""
+
     class Meta:
         model = Member
         fields = ['username', 'password']
--- user/urls.py
+++ user/urls.py
@@ -5,8 +5,8 @@ app_name = 'user'
 
 urlpatterns = [
     path('login/', views.u_login, name='u_login'),
-    path('signin/', views.u_signin, name='u_signin'),
-    path('signinSuccess', views.signin_success, name='signin_success'),
+    path('signup/', views.u_signup, name='u_signup'),
+    path('signupSuccess/', views.signup_success, name='signup_success'),
     path('loginProcess/', views.login_process, name='login_process'),
     path('logout/', views.u_logout, name='u_logout')
 ]
