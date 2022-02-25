dcfd1f130439f7630944038126e52124b506afbf
HKLeeeee <dlapdlf1739@gmail.com>
2022-02-25 13:08:33 +0900

    Alter Memeber Table

--- static/js/search_btn.js
+++ static/js/search_btn.js
@@ -1,5 +1,10 @@
 function search_func(){
-    $.ajax({
-
-    })
+    let results = document.search
+    /* if keyword 입력 x :
+     *      alert('검색어를 입력하세요')
+     *      return false
+     * else :
+     *      return true
+     */
+    alert(results)
 }
